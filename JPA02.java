687fb63164b0150ac2a6b8605c7a397cda90dd2f
like000777 <112b16995@mailst.cjcu.edu.tw>
2024-06-08 23:26:20 +0800

    Create JPA02.java

---
+++ JPA02.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class JPA02 {
+    static Scanner keyboard = new Scanner(System.in);
+    public static void main(String[] args) {
+        test();
+        test();
+        
+    }
+    public static void test() {
+        System.out.println("Input:"); 
+            int a,b;
+            a=keyboard.nextInt();
+            b=keyboard.nextInt();
+
+            if(a>b)
+                   System.out.printf("%d is large than %d\n",a,b);
+            
+            else if(b>a)
+                   System.out.printf("%d is large than %d\n",b,a);
+            
+            else
+                   System.out.printf("%d is equal to %d\n",a,b);
+            
+            
+        }
+
+        
+            
+
+
+    }
+       
+
+    
